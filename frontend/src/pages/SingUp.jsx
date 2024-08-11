aa10c46b696ca6480a56c771035a49a4e77f71aa
Amar Pisal <109473825+amarpisal8@users.noreply.github.com>
2024-08-11 22:30:39 +0530

    v1.0

---
+++ frontend/src/pages/SingUp.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../images/logo.png';
+const SingUp = () => {
+  return (
+    <>
+      <div className=' flex items-center justify-center flex-col h-screen bg-[#F0F0F0]'>
+        <div className="flex items-center">
+                <div className="left flex items-center justify-center flex-col ml-[50px]">
+
+                </div>
+                <div className="right">
+                    
+                </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SingUp
