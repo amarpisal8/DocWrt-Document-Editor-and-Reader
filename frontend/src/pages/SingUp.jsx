fe6b0ae89fcd84dba158148003f65b9113b72ba2
Amar Pisal <109473825+amarpisal8@users.noreply.github.com>
2024-08-11 23:29:00 +0530

    update v1.0.1

--- frontend/src/pages/SingUp.jsx
+++ frontend/src/pages/SingUp.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import logo from '../images/logo.png';
+import { FaUser } from "react-icons/fa";
 const SingUp = () => {
   return (
     <>
-      <div className=' flex items-center justify-center flex-col h-screen bg-[#F0F0F0]'>
+      <div className=' flex items-center w-screen justify-center flex-col h-screen bg-[#F0F0F0]'>
         <div className="flex items-center">
-                <div className="left flex items-center justify-center flex-col ml-[50px]">
+                <div className="left flex flex-col ml-[50px]">
                     <img src={logo} alt="Logo Icon" />
+                    <form className='pl-3 mt-5' action="">
+                        <div className="inputCon">
+                            <p className='text-[14px] text-[#808080]'>Username</p>
+                            <div className="inputBox w-[100%]">
+                                <i><FaUser/></i>
+                                <input type="text" name="username" id="username" placeholder='Enter Your Username' required/>
+                            </div>
+                        </div>
+                    </form>
                 </div>
                 <div className="right">
                     
